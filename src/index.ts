1b2f8257e5f49d0a6f92cb2518e30d69079e53eb
Fabian Quintanilla <fabian@avocadostudio.cl>
2022-06-29 22:57:13 -0400

    initial commit

---
+++ src/index.ts
@@ -0,0 +1,38 @@
+
+export const validateRut = (rut: any): boolean => {
+
+  if (typeof rut !== 'string' && typeof rut !== 'number') {
+    throw new Error("El tipo de dato es incorrecto");
+  }
+
+  let { rutSinDv, dVRut } = prepareRut(rut);
+  return getDvRut(rutSinDv) === dVRut.toLocaleLowerCase();
+
+};
+
+
+export const clearRut = (rut: string): string => rut.replace(/[^0-9a-z]/gi, '');
+
+const prepareRut = (rut: string | number) => {
+  const rutLimpio = (typeof rut === 'string' && clearRut(rut)) || '' + rut;
+  const rutSinDv = rutLimpio.slice(0, -1);
+  const dVRut = rutLimpio.split('').slice(-1)[0];
+  return { rutLimpio, rutSinDv, dVRut };
+}
+
+const getDvRut = (rut: string): string => {
+  const rRut = rut.split('').map(v => parseInt(v)).reverse();
+  let result = 0
+
+  for (let i = 0, j = 2; i < rRut.length; i++, j < 7 ? j++ : j = 2) {
+    result += rRut[i] * j;
+  }
+  result = 11 - (result % 11);
+  if (result == 11) return '0';
+  if (result == 10) return 'k';
+  return '' + result;
+}
+
+
+export const deleteDots = (rut: string): string => rut.replace(/\./g, '');
+export const deleteDash = (rut: string): string => rut.replace(/\-/g, '');
---
+++ test/validateRut.test.ts
@@ -0,0 +1,38 @@
+import { validateRut } from '../src';
+
+describe('validateRut', () => {
+  it('Rut numerico valido', () => {
+    expect(validateRut(933698688)).toEqual(true);
+  });
+
+  it('Rut numerico no valido', () => {
+    expect(validateRut(180769834)).toEqual(false);
+  });
+
+  it('Rut alfanumerico valido', () => {
+    expect(validateRut('90.384.078-1')).toEqual(true);
+  });
+
+  it('Rut alfanumerico no valido', () => {
+    expect(validateRut('90.384.078-0')).toEqual(false);
+  });
+
+  it('Rut con dv 0 valido', () => {
+    expect(validateRut(935767040)).toEqual(true);
+  });
+
+  it('Rut con dv k valido', () => {
+    expect(validateRut('98336313k')).toEqual(true);
+  });
+
+  it('Rut 19 valido', () => {
+    expect(validateRut('19')).toEqual(true);
+  });
+
+  it('Tipo de dato no valido', () => {
+    expect(function () {
+      validateRut({'19':0});
+    }).toThrow(new Error("El tipo de dato es incorrecto"));
+  });
+
+});
